894ebba9c1ead2d31b6eb280b52d619f19c363dc
nikhilmalakar <nikhilmalakarofficial@gmail.com>
2024-03-09 00:27:12 +0530

    Add: OneToOne and LiveStream API

--- Client/client/src/App.js
+++ Client/client/src/App.js
@@ -6,6 +6,9 @@ import {
 } from "react-router-dom";
 import SignupPage from './pages/register/Signup.js';
 import LoginPage from './pages/register/Login.js';
+import OneToOne from './pages/OneToOne.js';
+import LiveStreaming from './pages/LiveStreaming.js'
+// import Courses from './pages/courses/Courses.js';
 
 function App() {
   return (
@@ -15,6 +18,9 @@ function App() {
         <Routes>
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
+            <Route path="/onetoone" element={<OneToOne/>} />
+            <Route path="/livestream" element={<LiveStreaming/>} />
+            {/* <Route path="/courses" element={<Courses/>} /> */}
         </Routes>
       </BrowserRouter>
     </div>

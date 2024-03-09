aa798f895e095b79d779f11fbff3574897c502f7
Aman <amanjoshi149@gmail.com>
2024-03-09 17:38:17 +0530

    added payment using stripe

--- Server/controllers/subscription/addSubscription.js
+++ Server/controllers/subscription/addSubscription.js
@@ -2,6 +2,7 @@ import Subscription from "../../models/Subscription.js";
 import Tutor from "../../models/Tutor.js";
 import Student from "../../models/Student.js";
 import Language from "../../models/Language.js";
+import axios from "axios";
 
 const addSubscription = async (req, res) => {
     try {
@@ -9,6 +10,17 @@ const addSubscription = async (req, res) => {
         const tutor = await Tutor.findById(req.params.tid);
         const {languageId, stripeKey, duration, linkId,startTime,endTime} = req.body;
         const langid = await Language.findById(languageId);
+
+        
+        // call
+        // check success
+            // const response = await axios.post(
+            //   "http://localhost:3000/create-checkout-session",
+            //   stripeKey
+            // );
+
+        // 
+
         const newSubscription = new Subscription({
             studentId: student._id,
             tutorId: tutor._id,
@@ -25,8 +37,10 @@ const addSubscription = async (req, res) => {
         await tutor.save();
         await newSubscription.save();
         res.status(200).json({ message: "Subscription added successfully" });
+        axios.post("http://localhost:3000/create-checkout-session", stripeKey);
+
     } catch (error) {
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ message: error });
     }
 }
 
--- Server/index.js
+++ Server/index.js
@@ -4,12 +4,14 @@ import dbConnect from "./config/database.js";
 import authenticateToken from "./middleware/authMiddleware.js";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
+import stripe from "stripe";
 
 
 const app = express();
 
 dotenv.config();
 const port = process.env.PORT || 3000;
+const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 
 // Database connection
 dbConnect();
@@ -47,6 +49,32 @@ app.get("*", (req, res) => {
   app.redirect("/");
 }
 );
+
+app.post("/create-checkout-session", async (req, res) => {
+  const prices = await stripeInstance.prices.list({
+    lookup_keys: [req.body.lookup_key],
+    expand: ["data.product"],
+  });
+  const session = await stripeInstance.checkout.sessions.create({
+    billing_address_collection: "auto",
+    line_items: [
+      {
+        price: prices.data[0].id,
+        // For metered billing, do not pass quantity
+        quantity: 1,
+      },
+    ],
+    currency: "usd",
+    // customer: 'Madhusudan',
+    mode: "subscription",
+    success_url: `http://localhost:3000/?success=true&session_id=4040`,
+    cancel_url: `http://localhost:3000/?canceled=true`,
+  });
+  console.log("session:", session);
+
+  res.redirect(303, session.url);
+});
+
 
 // Start the server
 app.listen(port, () => {
